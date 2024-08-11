007f87acf398be7d2ed42f054766a61995378182
Abdi Deresa <Abdishegute@gmail.com>
2024-08-10 18:39:03 -0700

    added functions

---
+++ index.js
@@ -0,0 +1,11 @@
+//calculator program
+
+const display = document.getElementById("display");
+
+function appendTodisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {}
+
+function calculate() {}
